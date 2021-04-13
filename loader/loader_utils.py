487536ff8a76086a99459ebb8061f76fe4eb31ec
Ceyhun Ibolar <70475366+cibolar@users.noreply.github.com>
2021-04-13 15:40:08 +0300

    Update loader_utils.py

--- loader/loader_utils.py
+++ loader/loader_utils.py
@@ -7,7 +7,7 @@ def png_reader_32bit(path, img_size=(0,0)):
     image = Image.open(path)  
     pixel = np.array(image)
     if img_size[0]: #nearest interpolation
-        step = pixel.shape[0]/img_size[0]
+        step = pixel.shape[0]//img_size[0]
         pixel = pixel[0::step, :]
         pixel = pixel[:, 0::step]
 
